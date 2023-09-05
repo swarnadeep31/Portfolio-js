22b8d4939bc6ba6058485af6d6554b55f1ed53dc
swarndeep <swarnadeeproy69@gmail.com>
2023-09-05 12:20:39 +0530

    new1

--- app/components/Navebar.tsx
+++ app/components/Navebar.tsx
@@ -58,6 +58,17 @@ export default function Navebar() {
                   >
                     Blog
                   </Link>
+                  <Link
+                    href="/contact"
+                    prefetch
+                    className={`${
+                      pathname === "/contact"
+                        ? "border-teal-500 dark:text-white h-full inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                        : "border-transparent text-gray-500 dark:text-gray-300 dark:hover:text-white inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                    }`}
+                  >
+                    Contact
+                  </Link>
                   <Themebutton/>
                 </div>
               </div>
@@ -114,10 +125,10 @@ export default function Navebar() {
                 Home
               </Link>
               <Link
-                href="/Projects"
+                href="/projects"
                 prefetch
                 className={`${
-                  pathname == "/Projects"
+                  pathname == "/projects"
                     ? "bg-teal-50 border-teal-500 text-teal-500 block pl-3 pr-4 py-2 border-l-4 text-base font-medium dark:bg-gray-800"
                     : "border-transparent text-grat-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-white block pl-3 pr-4 py-2 dark:hover:bg-gray-700 border-l-4 text-base font-medium dark:text-white"
                 }`}
@@ -125,10 +136,21 @@ export default function Navebar() {
                 Projects
               </Link>
               <Link
-                href="/Contact"
+                href="/blog"
                 prefetch
                 className={`${
-                  pathname == "/Contact"
+                  pathname == "/blog"
+                    ? "bg-teal-50 border-teal-500 text-teal-500 block pl-3 pr-4 py-2 border-l-4 text-base font-medium dark:bg-gray-800"
+                    : "border-transparent text-grat-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-white block pl-3 pr-4 py-2 dark:hover:bg-gray-700 border-l-4 text-base font-medium dark:text-white"
+                }`}
+              >
+                Blog
+              </Link>
+              <Link
+                href="/contact"
+                prefetch
+                className={`${
+                  pathname == "/contact"
                     ? "bg-teal-50 border-teal-500 text-teal-500 block pl-3 pr-4 py-2 border-l-4 text-base font-medium dark:bg-gray-800"
                     : "border-transparent text-grat-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-white block pl-3 pr-4 py-2 dark:hover:bg-gray-700 border-l-4 text-base font-medium dark:text-white"
                 }`}
